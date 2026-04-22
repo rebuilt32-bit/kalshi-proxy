133ba00ec0fc7dd7193e9b4fa8e1e634facaa622
rebuilt32-bit <reliablelotus32@gmail.com>
2026-04-22 13:11:59 -0400

    Update kalshi-proxy-server.js

--- kalshi-proxy-server.js
+++ kalshi-proxy-server.js
@@ -1,65 +1,55 @@
-var https = require(“https”);
-var http = require(“http”);
+var https = require('https');
+var http = require('http');
 var PORT = process.env.PORT || 3001;
 
 http.createServer(function(req, res) {
-res.setHeader(“Access-Control-Allow-Origin”, “*”);
-res.setHeader(“Access-Control-Allow-Methods”, “GET,POST,PUT,DELETE,OPTIONS”);
-res.setHeader(“Access-Control-Allow-Headers”, “Content-Type,KALSHI-ACCESS-KEY,KALSHI-ACCESS-TIMESTAMP,KALSHI-ACCESS-SIGNATURE”);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,KALSHI-ACCESS-KEY,KALSHI-ACCESS-TIMESTAMP,KALSHI-ACCESS-SIGNATURE');
 
-if (req.method === “OPTIONS”) {
-res.writeHead(200);
-res.end();
-return;
-}
-
-var path = “/trade-api/v2” + req.url;
-var body = [];
-
-req.on(“data”, function(chunk) { body.push(chunk); });
-
-req.on(“end”, function() {
-var data = body.length ? Buffer.concat(body) : null;
-
-```
-var headers = {};
-var keys = Object.keys(req.headers);
-for (var i = 0; i < keys.length; i++) {
-  if (keys[i] !== "host") headers[keys[i]] = req.headers[keys[i]];
-}
-headers["host"] = "demo-api.kalshi.co";
-if (data) headers["content-length"] = Buffer.byteLength(data);
-
-var options = {
-  hostname: "demo-api.kalshi.co",
-  port: 443,
-  path: path,
-  method: req.method,
-  headers: headers
-};
-
-var proxy = https.request(options, function(upstream) {
-  var outHeaders = {};
-  var upKeys = Object.keys(upstream.headers);
-  for (var j = 0; j < upKeys.length; j++) {
-    outHeaders[upKeys[j]] = upstream.headers[upKeys[j]];
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
   }
-  outHeaders["Access-Control-Allow-Origin"] = "*";
-  res.writeHead(upstream.statusCode, outHeaders);
-  upstream.pipe(res);
-});
 
-proxy.on("error", function(err) {
-  res.writeHead(502);
-  res.end(JSON.stringify({ error: err.message }));
-});
+  var kalshiPath = '/trade-api/v2' + req.url;
+  var chunks = [];
 
-if (data) proxy.write(data);
-proxy.end();
-```
+  req.on('data', function(c) { chunks.push(c); });
 
-});
+  req.on('end', function() {
+    var body = chunks.length ? Buffer.concat(chunks) : null;
+    var h = {};
+    Object.keys(req.headers).forEach(function(k) {
+      if (k !== 'host') h[k] = req.headers[k];
+    });
+    h['host'] = 'demo-api.kalshi.co';
+    if (body) h['content-length'] = Buffer.byteLength(body);
+
+    var pr = https.request({
+      hostname: 'demo-api.kalshi.co',
+      port: 443,
+      path: kalshiPath,
+      method: req.method,
+      headers: h
+    }, function(up) {
+      var out = {};
+      Object.keys(up.headers).forEach(function(k) { out[k] = up.headers[k]; });
+      out['Access-Control-Allow-Origin'] = '*';
+      res.writeHead(up.statusCode, out);
+      up.pipe(res);
+    });
+
+    pr.on('error', function(e) {
+      res.writeHead(502);
+      res.end(e.message);
+    });
+
+    if (body) pr.write(body);
+    pr.end();
+  });
 
 }).listen(PORT, function() {
-console.log(“proxy running on port “ + PORT);
+  console.log('proxy on port ' + PORT);
 });
